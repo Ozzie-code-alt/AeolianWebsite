3ce5f148f7b331c1683872d8abd48f6ad44c272a
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-13 21:46:57 +0800

    Added Nav

--- AeolianWebsite/src/layout/Navbar.jsx
+++ AeolianWebsite/src/layout/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import '../layout/layout-styles/navbar.css'
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <div className="nav-main-container">
+<nav>
+  <ul>
+    <li>Aeolian</li>
+    <li> <button>Get Started</button></li>
+  </ul>
+</nav>
+
+      </div>
+
+    </div>
   )
 }
 
