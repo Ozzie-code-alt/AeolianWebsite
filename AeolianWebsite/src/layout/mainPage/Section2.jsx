fa3ea8931bc0109efc596d24dd6fe5804b644c67
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-15 17:07:33 +0800

    continue later

--- AeolianWebsite/src/layout/mainPage/Section2.jsx
+++ AeolianWebsite/src/layout/mainPage/Section2.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
-import '../../layout/layout-styles/section2.css'
+import React from "react";
+import "../../layout/layout-styles/section2.css";
 function Section2() {
   return (
-<div className='main-sec2-content'>
-<p>Section2</p>
+    <div className="main-sec2-content">
+      <div className="sec-2-container">
+        <div className="boxes-container">
+          <div className="box-1-container">
+            <div className="main-title-container">
+              <p>Intelligent Realtime Tracking using the OpenCV Framework</p>
+            </div>
+            <div className="sub-content-container">
+              Tracks the custom stylus using a combination of contour detection
+              and brightest pixel detection
+            </div>
+            <div className="sec2-btn-container">
+              <button>Learn more</button>
+            </div>
+          </div>
+          <div className="box-2-container">
+            <div className="main-title-container">
+              <p>Suppoerts Customization</p>
+            </div>
+            <div className="sub-content-container">
+       can choose between dark and light mode as well as a host of ther features to suit your needs
+            </div>
+            <div className="sec2-btn-container">
+              <button>Learn more</button>
+            </div>
+          </div>
+          <div className="box-3-container">
+            <div className="main-title-container">
+              <p>Smart Auto- Calibration</p>
+            </div>
+            <div className="sub-content-container">
+      Automatically generates an image mask which is compatible with the custom stylus
+            </div>
+            <div className="sec2-btn-container">
+              <button>Learn more</button>
+            </div>
+          </div>
 
-</div>
-  
-  )
+
+
+        </div>
+
+
+
+
+
+
+
+
+      </div>
+    </div>
+  );
 }
 
-export default Section2
+export default Section2;
