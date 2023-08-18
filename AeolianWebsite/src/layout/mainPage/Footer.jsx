4004f6bd844ec634a0eb7524909b481b9dd5b48d
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-18 23:23:24 +0800

    FooterChanges

--- AeolianWebsite/src/layout/mainPage/Footer.jsx
+++ AeolianWebsite/src/layout/mainPage/Footer.jsx
@@ -16,9 +16,41 @@ function Footer() {
                 </div>
         </div>
 
-        <div className="submit-request-container"></div>
-        <div className="learn-more-container"></div>
-        <div className="about-me-container"></div>
+        <div className="submit-request-container">
+        <div className="title-container">
+            <p>Submit Request</p>
+            </div>
+                <div className="description-container">
+                    <p>Submit a request or inform me of any bugs</p>
+                </div>
+                <div className="link-container">
+                    <p>Send a Request</p>
+                </div>
+
+        </div>
+        <div className="learn-more-container">
+
+        <div className="title-container">
+            <p>Learn More</p>
+            </div>
+                <div className="description-container">
+                    <p>What is Aeolian and what does it DO ?</p>
+                </div>
+                <div className="link-container">
+                    <p>Find out more</p>
+                </div>
+        </div>
+        <div className="about-me-container">
+        <div className="title-container">
+            <p>About Me</p>
+            </div>
+                <div className="description-container">
+                    <p>A little but about me and what i do</p>
+                </div>
+                <div className="link-container">
+                    <p>About Me</p>
+                </div>
+        </div>
       </div>
       <div className="lower-grid-container"></div>
     </div>
