0c98ecae9dbffb970ce0f45e217abed9cfeab3a3
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-23 22:26:29 +0800

    MidContainer

---
+++ AeolianWebsite/src/layout/getAeolian/MidContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../layout/layout-styles/midContainer.css'
+function MidContainer() {
+  return (
+    <div className='midContainer-container'>
+        <div className="main-title-container">
+            <p>Requirements for Source Code </p>
+        </div>
+        <div className="sub-text-container">
+            <p>The following is <b>only</b> required if you are downloading the source code</p>
+        </div>
+    </div>
+  )
+}
+
+export default MidContainer
--- AeolianWebsite/src/pages/getAeolian.jsx
+++ AeolianWebsite/src/pages/getAeolian.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../styles/getAeolian.css'
 import UpperContainer from '../layout/getAeolian/UpperContainer'
+import MidContainer from '../layout/getAeolian/midContainer'
 function GetAeolian() {
   return (
 
     <div className='get-main-container'>
       <UpperContainer/>
-
+      <MidContainer/>
       </div>
  
   )
