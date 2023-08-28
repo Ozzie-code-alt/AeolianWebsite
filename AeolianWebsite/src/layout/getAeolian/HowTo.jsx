f7c207e17e6429fd5fd4ee7dc55e0b63ca7086eb
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-28 15:39:16 +0800

    somechanges

---
+++ AeolianWebsite/src/layout/getAeolian/HowTo.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../layout/layout-styles/howTo.css'
+function HowTo() {
+
+  return (
+    <section className='how-to-container'>
+          <div className="description-container">
+            <div className="title-container">
+                <p>How to download the packages</p>
+            </div>
+            
+          </div>
+
+          <div className="picture-container">
+
+          </div>
+          
+    </section>
+  )
+}
+
+export default HowTo
--- AeolianWebsite/src/pages/getAeolian.jsx
+++ AeolianWebsite/src/pages/getAeolian.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import '../styles/getAeolian.css'
 import UpperContainer from '../layout/getAeolian/UpperContainer'
 import MidContainer from '../layout/getAeolian/midContainer'
+import HowTo from '../layout/getAeolian/HowTo'
 function GetAeolian() {
   return (
 
     <div className='get-main-container'>
       <UpperContainer/>
       <MidContainer/>
+      <HowTo/>
+      
       </div>
  
   )
